e7672d5eba8238c369cdbc75bb2c6b54dade7840
qbsb1 <qbsb147@naver.com>
2024-08-12 06:25:40 +0900

    수정

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -20,7 +20,7 @@ public class SampleController {
     @GetMapping("/hello")
     public void hello(Model model) {
 
-        log.info("hello................");
+        log.info("hello.......qdw.........");
 
         model.addAttribute("msg", "HELLO WORLD");
     }
